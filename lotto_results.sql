2d06cbc3c241ad2a4c14f10c3ddb4035be9379c5
Roksia <142537084+Roksia33@users.noreply.github.com>
2023-10-01 17:10:26 +0000

    Table update

--- lotto_results.sql
+++ lotto_results.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 28, 2023 at 11:41 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Generation Time: Paź 01, 2023 at 07:09 PM
+-- Wersja serwera: 10.4.28-MariaDB
+-- Wersja PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,15 +24,15 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `lotto_results`
+-- Struktura tabeli dla tabeli `lotto_results`
 --
 
 CREATE TABLE `lotto_results` (
   `id` int(11) NOT NULL,
-  `lotto_type` text NOT NULL,
+  `lotto_type` text CHARACTER SET utf16 COLLATE utf16_polish_ci NOT NULL,
   `number1` int(11) NOT NULL,
-  `number2` int(11) NOT NULL,
-  `number3` int(11) NOT NULL,
+  `number2` int(11) DEFAULT NULL,
+  `number3` int(11) DEFAULT NULL,
   `number4` int(11) DEFAULT NULL,
   `number5` int(11) DEFAULT NULL,
   `number6` int(11) DEFAULT NULL,
@@ -41,23 +41,17 @@ CREATE TABLE `lotto_results` (
   `number9` int(11) DEFAULT NULL,
   `number10` int(11) DEFAULT NULL,
   `number11` int(11) DEFAULT NULL,
-  `number12` int(11) DEFAULT NULL
+  `number12` int(11) DEFAULT NULL,
+  `e_number1` int(11) DEFAULT NULL,
+  `e_number2` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 --
--- Dumping data for table `lotto_results`
---
-
-INSERT INTO `lotto_results` (`id`, `lotto_type`, `number1`, `number2`, `number3`, `number4`, `number5`, `number6`, `number7`, `number8`, `number9`, `number10`, `number11`, `number12`) VALUES
-(1, 'Lotto', 34, 1, 44, 11, 2, 17, 23, 56, 45, 22, 21, 13),
-(2, 'Lotto', 43, 1, 41, 45, 23, 8, 3, 67, 10, 24, 15, 19);
-
---
--- Indexes for dumped tables
+-- Indeksy dla zrzutów tabel
 --
 
 --
--- Indexes for table `lotto_results`
+-- Indeksy dla tabeli `lotto_results`
 --
 ALTER TABLE `lotto_results`
   ADD PRIMARY KEY (`id`);
@@ -70,7 +64,7 @@ ALTER TABLE `lotto_results`
 -- AUTO_INCREMENT for table `lotto_results`
 --
 ALTER TABLE `lotto_results`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
