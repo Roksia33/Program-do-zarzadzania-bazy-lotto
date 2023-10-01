7b7c71dde2accda557b7dae5655eb0091bd2e357
Roksia <142537084+Roksia33@users.noreply.github.com>
2023-10-01 16:58:42 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,52 @@
 import tkinter as tk
-from tkinter import NO, W, ttk
-from tkinter import messagebox
+from tkinter import NO, W, ttk, messagebox
 import datetime
 import mysql.connector
+import subprocess
 
+####### funkcja do wyśrodkowania głównego okna #######
+def center_window(window, width, height):
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+    x = (screen_width - width) // 2
+    y = (screen_height - height) // 2
+    window.geometry(f"{width}x{height}+{x}+{y}")
 
-####### Inicjalizacja głównego okna #######
+####### tworzenie głównego okna #######
 root = tk.Tk()
-root.title("Aplikacja Lotto")
-root.geometry("800x940")
+root.title("Aplikacja do zarządzania bazą Lotto")
+window_width = 800
+window_height = 940
+center_window(root, window_width, window_height)
 root.resizable(False,False)
+
+####### Lotto #######
+def Lotto():
+    subprocess.Popen(["python", "Lotto.py"])
+
+####### Eurojackpot #######
+def Eurojackpot():
+    subprocess.Popen(["python", "Eurojackpot.py"])
+
+####### Keno #######
+def Keno():
+    subprocess.Popen(["python", "Keno.py"])
+
+####### Szybkie 600 #######
+def Szybkie_600():
+    subprocess.Popen(["python", "Szybkie_600.py"])
+
+####### Multi Multi #######
+def Multi_Multi():
+    subprocess.Popen(["python", "Multi_multi.py"])
+
+####### Ekstra Pensja #######
+def Ekstra_Pensja():
+    subprocess.Popen(["python", "Ekstra_pensja.py"])
+
+####### Mini Lotto #######
+def Mini_Lotto():
+    subprocess.Popen(["python", "Mini_lotto.py"])
 
 ####### Funkcja do wyświetlania aktualnej godziny i daty na pierwszej zakładce #######
 def display_time_date():
@@ -19,108 +56,15 @@ def display_time_date():
     label_time_date.config(text=f"Aktualna godzina: {current_time}\nAktualna data: {current_date}")
     root.after(1000, display_time_date)
 
-####### Funkcja do obsługi zapisywania danych do bazy MySQL #######
-def save_to_database():
-    # Połączenie z bazą danych
-    conn = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="lotto"
-    )
-    
-    # Sprawdzenie połączenia
-    if conn.is_connected():
-        cursor = conn.cursor()
-        
-        # Pobranie danych z pól wprowadzania
-        lotto_type = entry_lotto_type.get()
-        numbers = [entry_num.get() for entry_num in entry_numbers]
-        
-        # Zapisanie danych do bazy
-        try:
-            cursor.execute("INSERT INTO lotto_results (lotto_type, number1, number2, number3, number4, number5, number6, number7, number8, number9, number10, number11, number12) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
-                           (lotto_type,) + tuple(numbers))
-            conn.commit()
-            messagebox.showinfo("Sukces", "Dane zostały zapisane do bazy danych.")
-        except Exception as e:
-            conn.rollback()
-            messagebox.showerror("Błąd", f"Wystąpił błąd podczas zapisywania danych: {e}")
-        
-        # Zamknięcie połączenia
-        conn.close()
-
-####### Funkcja do odświeżania wprowadzonych danych #######
-def refresh_data():
-    entry_lotto_type.delete(0, tk.END)
-    for entry_num in entry_numbers:
-        entry_num.delete(0, tk.END)
-
 ####### Funkcja do wyświetlania powtarzających się liczb #######
 def check_duplicates():
-    
-    # Połącz z bazą danych MySQL
-    connection = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="",
-    database="lotto"
-    )
-    
-    cursor = connection.cursor()
-    
-    # Inicjalizacja słownika do przechowywania wyników
-    number_counts = {}
-    
-    # Pobierz nazwy kolumn w tabeli lotto_results
-    cursor.execute("DESCRIBE lotto_results")
-    column_names = [row[0] for row in cursor.fetchall()]
-    
-    # Iteruj przez kolumny z numerami (number1, number2, ..., number12)
-    for column_name in column_names:
-        if column_name.startswith("number"):
-            # Zapytanie SQL do zliczenia wystąpień liczby w danej kolumnie
-            query = f"SELECT {column_name}, COUNT(*) as count FROM lotto_results GROUP BY {column_name}"
-            cursor.execute(query)
-            
-            # Pobierz wyniki i zaktualizuj słownik
-            results = cursor.fetchall()
-            for number, count in results:
-                if number in number_counts:
-                    number_counts[number] += count
-                else:
-                    number_counts[number] = count
-    
-    # Zamknij połączenie z bazą danych
-    connection.close()
-    
-    return number_counts
-
-# Tworzenie okna głównego
-check = tk.Toplevel(root)
-check.title("Tabela z bazy danych")
-check.geometry("1000x600")
-
-# Ustal rozmiar okna
-check.geometry("600x700")
-
-# Zlicz wystąpienia liczby
-number_counts = check_duplicates()
-
-# Tworzenie etykiety do wyświetlenia wyników
-label = tk.Label(check, text="Liczba wystąpień w bazie:")
-label.pack()
-
-# Wyświetlenie wyników
-for number, count in number_counts.items():
-    result_label = tk.Label(check, text=f"Liczba {number}: {count} wystąpień")
-    result_label.pack()
-
+    subprocess.Popen(["python", "LA.py"])
 
 ####### Funkcja do modyfikacji tabeli w bazie danych #######
 def update_database():
-    print()
+    subprocess.Popen(["python", "zmiana_danych.py"])
 
+####### Funkcja do wyświetlania tabeli w bazie danych #######
 def show_database_window():
      # Połączenie z bazą danych MySQL
     mydb = mysql.connector.connect(
@@ -137,15 +81,16 @@ def show_database_window():
 
     # Tworzenie nowego okna
     database_window = tk.Toplevel(root)
-    database_window.title("Tabela z bazy danych")
-    database_window.geometry("1000x600")
+    database_window.title("")
+    database_window.geometry("1200x600")
+    database_window.resizable(False,False)
 
     # Tworzenie tabelki do wyświetlenia danych z bazy
     tree = ttk.Treeview(database_window, columns=list(range(len(mycursor.column_names))), show="headings")
     
     # Ustalanie nagłówków kolumn
     for i, col_name in enumerate(mycursor.column_names):
-        tree.column(i,anchor=W,width=70)
+        tree.column(i,anchor=tk.CENTER,width=80)
         tree.heading(i, text=col_name)
     
     # Wyświetlanie danych
@@ -154,11 +99,19 @@ def show_database_window():
 
     tree.pack(fill="both", expand=True)
 
+####### Funkcja do sprawdzania ilości rekordów w bazie danych #######
+def check_records():
+    subprocess.Popen(["python", "Slr.py"])
+
+####### Funkcja do wyświetlania ostatnich powtarzających się liczb w bazie danych #######
+def check_l():
+    subprocess.Popen(["python", "spl.py"])
+
 ####### Zakładki #######
 notebook = ttk.Notebook(root)
 notebook.pack(fill=tk.BOTH, expand=True)
 
-####### Zakładka 1: Ekran powitania #######
+####### Zakładka 1: Ekran startowy #######
 tab1 = tk.Frame(notebook)
 notebook.add(tab1, text="Strona główna")
 
@@ -169,61 +122,76 @@ label_time_date.pack(pady=20)
 
 display_time_date()
 
-image = tk.PhotoImage(file="lotto.png")
+image = tk.PhotoImage(file=r"lotto.png")
 image_label = tk.Label(tab1, image=image)
 image_label.image = image
 image_label.pack(pady=20)
 
+V_label = tk.Label(tab1, text="Ver. 1.0.0", font=("Helvetica", 15))
+V_label.pack(pady=50)
+
 ####### Zakładka 2: Wprowadzanie danych #######
 tab2 = tk.Frame(notebook)
-label = tk.Label(tab2, text="Wprowadzanie danych Lotto", font=("Helvetica", 18))
+label = tk.Label(tab2, text="Wybierz typ Lotto", font=("Helvetica", 25))
 label.pack(pady=20)
 
-notebook.add(tab2, text="Wprowadź dane")
+notebook.add(tab2, text="Dodaj dane")
 
-label_lotto_type = tk.Label(tab2, text="Typ Lotto:", font=("Helvetica", 14))
-label_lotto_type.pack(pady=5)
 
-entry_lotto_type = tk.Entry(tab2)
-entry_lotto_type.pack(pady=10)
+button_lotto = tk.Button(tab2, text="Lotto", height = 7, width = 20, bg='#3cc6e0', command=Lotto, font=("Helvetica", 16))
+button_lotto.place(x=15,y=90)
 
-entry_numbers = []
-for i in range(12):
-    label = tk.Label(tab2, text=f"Numer {i+1}:", font=("Helvetica", 14))
-    label.pack(pady=5)
-    entry_num = tk.Entry(tab2)
-    entry_num.pack(pady=2)
-    entry_numbers.append(entry_num)
+button_eurojackpot = tk.Button(tab2, text="Eurojackpot", height = 7, width = 20, bg='#e0c05f', command=Eurojackpot, font=("Helvetica", 16))
+button_eurojackpot.place(x=275,y=90)
 
-button_save = tk.Button(tab2, text="Zapisz do bazy", height = 3, width = 15, bg='#ffb92d', command=save_to_database)
-button_refresh = tk.Button(tab2, text="Odśwież", height = 3, width = 15, bg='#ffb92d', command=refresh_data)
-button_save.place(x=590, y=100)
-button_refresh.place(x=590, y=170)
+button_keno = tk.Button(tab2, text="Keno", height = 7, width = 20, bg='#fc851a', command=Keno, font=("Helvetica", 16))
+button_keno.place(x=535,y=90)
+
+button_szybkie_600 = tk.Button(tab2, text="Szybkie 600", height = 7, width = 20, bg='#f49504', command=Szybkie_600, font=("Helvetica", 16))
+button_szybkie_600.place(x=15,y=290)
+
+button_multi_multi = tk.Button(tab2, text="Multi Multi", height = 7, width = 20, bg='#bf22af', command=Multi_Multi, font=("Helvetica", 16))
+button_multi_multi.place(x=275,y=290)
+
+button_ekstra_pensja = tk.Button(tab2, text="Ekstra Pensja", height = 7, width = 20, bg='#6aad32', command=Ekstra_Pensja, font=("Helvetica", 16))
+button_ekstra_pensja.place(x=535,y=290)
+
+button_mini_lotto = tk.Button(tab2, text="Mini Lotto", height = 7, width = 20, bg='#fdca51', command=Mini_Lotto, font=("Helvetica", 16))
+button_mini_lotto.place(x=275,y=490)
 
 
 ####### Zakładka 3: Zarządzanie bazą #######
 tab3 = tk.Frame(notebook)
 notebook.add(tab3, text="Zarządzanie bazą")
 label = tk.Label(tab3, text="Zarządzaj bazą", font=("Helvetica", 25))
-label.pack(pady=80)
+label.pack(pady=20)
 
-button_check_duplicates = tk.Button(tab3, text="Sprawdź powtórzenia", height = 3, width = 20, bg='#ffb92d', command=check_duplicates, font=("Helvetica", 16))
+button_check_duplicates = tk.Button(tab3, text="Sprawdź powtórzenia", height = 3, width = 25, bg='#ffb92d', command=check_duplicates, font=("Helvetica", 16))
 button_check_duplicates.pack(pady=10)
 
+button_check_l = tk.Button(tab3, text="Sprawdź ostatnie powtórzenia", height = 3, width = 25, bg='#ffb92d', command=check_l, font=("Helvetica", 16))
+button_check_l.pack(pady=10)
 
-button_update = tk.Button(tab3, text="Aktualizuj", height = 3, width = 20, bg='#ffb92d', command=update_database, font=("Helvetica", 16))
+button_check_records = tk.Button(tab3, text="Sprawdź liczbę rekordów", height = 3, width = 25, bg='#ffb92d', command=check_records, font=("Helvetica", 16))
+button_check_records.pack(pady=10)
+
+
+button_update = tk.Button(tab3, text="Zmień dane", height = 3, width = 25, bg='#ffb92d', command=update_database, font=("Helvetica", 16))
 button_update.pack(pady=10)
 
-open_database_button = tk.Button(tab3, text="Otwórz bazę danych", height = 3, width = 20, bg='#ffb92d', command=show_database_window, font=("Helvetica", 16))
+open_database_button = tk.Button(tab3, text="Otwórz bazę danych", height = 3, width = 25, bg='#ffb92d', command=show_database_window, font=("Helvetica", 16))
 open_database_button.pack(pady=10)
 
 
-####### Zakładka 4: Wyjście #######
+####### Zakładka 4: Menu #######
 tab4 = tk.Frame(notebook)
-notebook.add(tab4, text="Wyjście")
+notebook.add(tab4, text="Menu")
 
-button_exit = tk.Button(tab4, text="Wyjście",height = 30, width = 30, bg='#ffb92d', command=root.quit, font=("Helvetica", 30))
-button_exit.pack(pady=400)
+label = tk.Label(tab4, text="Menu", font=("Helvetica", 25))
+label.pack(pady=20)
+
+button_exit = tk.Button(tab4, text="Wyjście",height = 3, width = 25, bg='#ffb92d', command=root.quit, font=("Helvetica", 16))
+button_exit.pack(pady=10)
 
 ####### Uruchomienie aplikacji #######
 if __name__ == '__main__':
